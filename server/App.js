877bf04a336d3608486752152926fb86695e5caa
Debayan  Pal <94439470+Debayan1811arka@users.noreply.github.com>
2025-07-29 18:43:50 +0530

    Update App.js

--- server/App.js
+++ server/App.js
@@ -7,7 +7,10 @@ const router = require("./Routes/router")
 const port = 6002;
 
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://authenport.netlify.app/',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use(router)
