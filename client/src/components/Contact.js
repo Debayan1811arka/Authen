6b379afb08503eace5cd93aa1829450db709bd58
Debayan  Pal <94439470+Debayan1811arka@users.noreply.github.com>
2025-07-31 11:00:38 +0530

    Update Contact.js

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -33,7 +33,7 @@ const Contact = () => {
     if (!mobile) return toast.error('Mobile Number is required');
 
     try {
-      const res = await fetch('https://authen-tz7a.onrender.com/register', {
+      const res = await fetch('https://authen-tz7a.onrender.com/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
